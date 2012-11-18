1c6e200bca0dd7d4513c2c8743756f6e0c885cf0
Jack Brewer <jack@jackbrewer.co.uk>
2012-11-18 23:47:33 +0000

    Replacing minified JS with unminified versions

--- js/clock.js
+++ js/clock.js
@@ -1,1 +1,66 @@
-function initClock(){clock();setInterval(clock,100)}function clock(){var e=new Date,t=document.getElementById("icon-clock").getContext("2d"),n=60;t.save();t.clearRect(0,0,n,n);t.translate(n/2,n/2);t.scale(.5,.5);t.rotate(-Math.PI/2);t.strokeStyle="#000";t.fillStyle="#000";t.lineCap="round";t.lineWidth=n*.2;var r=e.getSeconds(),i=e.getMinutes(),s=e.getHours(),o=e.getMilliseconds();s=s>=12?s-12:s;t.save();t.rotate(s*(Math.PI/6)+Math.PI/360*i+Math.PI/21600*r);t.beginPath();t.moveTo(0,0);t.lineTo(n*.5,0);t.stroke();t.restore();t.save();t.rotate(Math.PI/30*i+Math.PI/1800*r);t.beginPath();t.moveTo(0,0);t.lineTo(n*.66,0);t.stroke();t.restore();t.save();t.rotate((r+o/999)*Math.PI/30);t.lineWidth=n/10;t.beginPath();t.moveTo(0,0);t.lineTo(n*.7,0);t.stroke();t.restore();t.globalCompositeOperation="source-out";t.beginPath();t.fillStyle="#fff";t.arc(0,0,n,0,Math.PI*2,!0);t.fill();t.restore()};
+function initClock(){
+  clock()
+  setInterval(clock, 100)
+}
+
+function clock(){
+
+  var now = new Date()
+    , ctx = document.getElementById('icon-clock').getContext('2d')
+    , baseSize = 60
+
+  // General Setup
+  ctx.save()
+  ctx.clearRect(0, 0, baseSize, baseSize)
+  ctx.translate(baseSize / 2, baseSize / 2)
+  ctx.scale(0.5, 0.5)
+  ctx.rotate(-Math.PI/2)
+  ctx.strokeStyle = '#000'
+  ctx.fillStyle = '#000'
+  ctx.lineCap = 'round'
+  ctx.lineWidth = baseSize * 0.2
+
+  // Get the Time
+  var sec = now.getSeconds()
+    , min = now.getMinutes()
+    , hr  = now.getHours()
+    , milli = now.getMilliseconds()
+
+  hr = hr >= 12 ? hr - 12 : hr
+
+  // Write Hours
+  ctx.save()
+  ctx.rotate( hr*(Math.PI/6) + (Math.PI/360)*min + (Math.PI/21600)*sec )
+  ctx.beginPath()
+  ctx.moveTo(0, 0)
+  ctx.lineTo(baseSize * 0.5, 0)
+  ctx.stroke()
+  ctx.restore()
+
+  // Write Minutes
+  ctx.save()
+  ctx.rotate( (Math.PI/30)*min + (Math.PI/1800)*sec )
+  ctx.beginPath()
+  ctx.moveTo(0, 0)
+  ctx.lineTo(baseSize * 0.66, 0)
+  ctx.stroke()
+  ctx.restore()
+
+  // Write seconds
+  ctx.save()
+  ctx.rotate( (sec + (milli / 999)) * Math.PI/30)
+  ctx.lineWidth = baseSize / 10
+  ctx.beginPath()
+  ctx.moveTo(0, 0)
+  ctx.lineTo(baseSize * 0.7, 0)
+  ctx.stroke()
+  ctx.restore()
+
+  ctx.globalCompositeOperation = 'source-out'
+  ctx.beginPath()
+  ctx.fillStyle = '#fff'
+  ctx.arc(0, 0, baseSize, 0, Math.PI*2, true)
+  ctx.fill()
+
+  ctx.restore()
+}
--- js/datepicker.js
+++ js/datepicker.js
@@ -17,4 +17,316 @@
 //  - maybe add an 'on this day' easter egg
 //  - make overlay work (finish JS loading work before reveal)
 //
-function datepicker(){function n(n,u){u=r(n,u);u=u.toString();u=i(n,u);e[n].value=u;s(n,u);o(n,u);var a=new Date(e.month.value+"/"+e.day.value+"/"+e.year.value);if(!isNaN(a)){a=t[a.getDay()];$(".datepicker-weekday").text(""+a+"")}}function r(t,n){var r=e[t].min,i=e[t].max;n=parseInt(n,10)||n==="0"||n==="00"?parseInt(n,10):n;typeof n!="number"&&(n=e[t].value);if(n>=r&&n<=i)return n;if(n<r)return r;if(n>i)return i}function i(t,n){var r=e[t].length;return n.length<r?i(t,"0"+n):n}function s(t){var n=e[t].value;$(".datepicker-form-"+t).attr("value",n)}function o(t){var n=$(".datepicker-unit-list[data-unit="+t+"]"),r=e[t].value;n.find(".active").removeClass("active");n.find('[data-value="'+r+'"]').addClass("active")}function u(e){var t=e&&e!==""?new Date(e):new Date;n("day",t.getDate());n("month",t.getMonth()+1);n("year",t.getFullYear());n("hour",t.getHours());n("minute",t.getMinutes());$(".datepicker-form-day").select()}function a(){var t="Your chosen date is ";t+=e.day.value+"/";t+=e.month.value+"/";t+=e.year.value+" - ";t+=e.hour.value+":";t+=e.minute.value;return t}var e={day:{value:"",min:1,max:31,length:2},month:{value:"",min:1,max:12,length:2},year:{value:"",min:1900,max:2100,length:4},hour:{value:"",min:0,max:23,length:2},minute:{value:"",min:0,max:59,length:2}},t=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];u();$(".datepicker-row li a").on("click",function(e){e.preventDefault();var t=$(this).closest("ul").data("unit"),r=$(this).data("value");n(t,r)});$('.datepicker-form-row input[class*="datepicker-form-"]').on("blur",function(){var e=$(this).data("unit"),t=$(this).attr("value");n(e,t)});$(".datepicker-presets a").on("click",function(){function n(e){var t=new Date,n=new Date(t.getFullYear(),t.getMonth(),t.getDate()+e,t.getHours(),t.getMinutes());return n}var e=$(this).data("preset-date"),t=new Date(e);if(t.getFullYear()>0)u(e);else{e.toLowerCase()==="today"&&u();if(e.match(/(.* days)/i)){e=parseInt(e,10);u(n(e))}}});$('input[type="button"]').on("click",function(){if((e.month.value==="04"||e.month.value==="06"||e.month.value==="09"||e.month.value==="11")&&e.day.value==="31"){alert("Your chosen month doesn't have 31 days!");return!1}console.log(e.month.value);if(e.month.value==="02"){var t=e.year.value%4===0&&(e.year.value%100!==0||e.year.value%400===0);if(e.day.value>"29"||e.day.value==="29"&&!t){alert("February "+e.year.value+" doesn't have "+e.day.value+" days!");return!1}}alert(a())})}(function(){datepicker()})();
+
+
+function datepicker(){
+
+  var dateTime = {
+        day: {
+            value: ''
+          , min: 1
+          , max: 31
+          , length: 2
+        }
+      , month: {
+            value: ''
+          , min: 1
+          , max: 12
+          , length: 2
+        }
+      , year: {
+            value: ''
+          , min: 1900
+          , max: 2100
+          , length: 4
+        }
+      , hour: {
+            value: ''
+          , min: 0
+          , max: 23
+          , length: 2
+        }
+      , minute: {
+            value: ''
+          , min: 0
+          , max: 59
+          , length: 2
+        }
+      }
+    , weekdays = [
+        'Sunday'
+      , 'Monday'
+      , 'Tuesday'
+      , 'Wednesday'
+      , 'Thursday'
+      , 'Friday'
+      , 'Saturday'
+      ]
+
+
+  //
+  // UPDATE VALUE
+  // This is the only place the dateTime object should be updated
+  //
+
+  function updateValue(unit, value){
+
+    value = validateValue(unit, value)
+
+    value = value.toString()
+
+    value = padValue(unit, value)
+
+    dateTime[unit].value = value
+
+    setInputValue(unit, value)
+    setActiveLink(unit, value)
+
+    var dayName = new Date(dateTime.month.value + '/' + dateTime.day.value + '/' + dateTime.year.value)
+
+    // Add day name
+    if (!isNaN(dayName)) {
+      dayName = weekdays[dayName.getDay()]
+      $('.datepicker-weekday').text('' + dayName + '')
+    }
+
+  }
+
+
+  //
+  // VALIDATE VALUE
+  // Ensures the value returned is between the unit's min and max properties
+  // If the value received is not a number, default to the min value
+  //
+
+  function validateValue(unit, value){
+
+    var minValue = dateTime[unit].min
+      , maxValue = dateTime[unit].max
+
+    // Ensure any strings are converted to integers where possible
+    // The checks for zeros need refactoring
+    value = parseInt(value, 10) || value === '0' || value === '00' ? parseInt(value, 10) : value
+
+    // If value is still not an integer, set it back to the existing value
+    if ( typeof value !== 'number'){
+      value = dateTime[unit].value
+    }
+
+    if (value >= minValue && value <= maxValue) {
+      return value
+    } else if (value < minValue) {
+      return minValue
+    } else if (value > maxValue) {
+      return maxValue
+    }
+
+  }
+
+
+  //
+  // ZERO PADDING
+  // Check required max unit length and zero pad until reached
+  //
+
+  function padValue (unit, value) {
+
+    var length = dateTime[unit].length
+
+    return value.length < length ? padValue(unit, '0' + value) : value
+
+  }
+
+
+  //
+  // SET INPUT VALUE
+  //
+
+  function setInputValue(unit){
+
+    // Find the new value
+    var newValue = dateTime[unit].value
+
+    // Update the input
+    $('.datepicker-form-' + unit).attr('value', newValue)
+
+  }
+
+
+  //
+  // SetActiveLink
+  //
+
+  function setActiveLink(unit) {
+
+    // Find the unit's list of links
+    var unitList = $('.datepicker-unit-list[data-unit=' + unit + ']')
+      , newValue = dateTime[unit].value
+
+    // Unset any active classes already set
+    unitList.find('.active').removeClass('active')
+
+    unitList.find('[data-value="' + newValue + '"]').addClass('active')
+
+  }
+
+
+  //
+  // SET DATE AND TIME
+  // Sets the datepicker to a specific date and time.
+  // Accepts custom dates and defaults to now.
+  //
+
+  function setDateTime(suppliedDate) {
+
+    var newDate = suppliedDate && suppliedDate !== '' ? new Date(suppliedDate) : new Date()
+
+    updateValue('day',    newDate.getDate()     )
+    updateValue('month',  newDate.getMonth() + 1) // Months are zero-based
+    updateValue('year',   newDate.getFullYear() )
+    updateValue('hour',   newDate.getHours()    )
+    updateValue('minute', newDate.getMinutes()  )
+
+    $('.datepicker-form-day').select()
+
+  }
+
+
+  //
+  // CONSTRUCT DATE
+  //
+
+  function constructDate() {
+
+    var completeDate = 'Your chosen date is '
+
+    completeDate += dateTime.day.value + '/'
+    completeDate += dateTime.month.value + '/'
+    completeDate += dateTime.year.value + ' - '
+    completeDate += dateTime.hour.value + ':'
+    completeDate += dateTime.minute.value
+
+    return completeDate
+
+  }
+
+
+  //
+  // SET INITIAL VALUE
+  //
+
+  setDateTime()
+
+
+
+  //
+  // USER INTERACTION
+  // ================
+  //
+
+
+  //
+  // When a link is clicked
+  //
+
+  $('.datepicker-row li a').on('click', function(e){
+
+    e.preventDefault()
+
+    var unit = $(this).closest('ul').data('unit')
+      , value = $(this).data('value')
+
+    updateValue(unit, value)
+
+  })
+
+
+  //
+  // When a form value is updated
+  //
+
+  $('.datepicker-form-row input[class*="datepicker-form-"]').on('blur',function(){
+
+    var unit = $(this).data('unit')
+      , value = $(this).attr('value')
+
+    updateValue(unit, value)
+
+  })
+
+
+  //
+  // PRESET DATES
+  // In the demo, I've hard coded 2 custom examples. Passing actual dates works.
+  // This could be improved to take many keywords and relative dates as input
+  // E.g. Yesterday, 2 weeks ago, next year.
+  //
+
+  $('.datepicker-presets a').on('click', function(){
+
+    var newDate = $(this).data('preset-date')
+
+    var testDate = new Date(newDate)
+
+    if ( testDate.getFullYear() > 0 ) {
+      setDateTime(newDate)
+    } else {
+
+      if (newDate.toLowerCase() === 'today') {
+        setDateTime()
+      }
+
+      if (newDate.match(/(.* days)/i) ) {
+
+        newDate = parseInt(newDate, 10)
+        setDateTime(futureDate( newDate ))
+      }
+
+    }
+
+
+    //
+    // FORMATTING PAST / FUTURE DATES
+    //
+
+    function futureDate( days ){
+      var today = new Date();
+      var nextweek = new Date(today.getFullYear(), today.getMonth(), today.getDate() + days, today.getHours(), today.getMinutes());
+      return nextweek;
+    }
+
+
+  })
+
+
+  //
+  // On submit - DEMO ONLY
+  //
+
+  $('input[type="button"]').on('click', function(){
+
+    // Check day is in month
+    if ((dateTime.month.value === '04' || dateTime.month.value === '06' || dateTime.month.value === '09' || dateTime.month.value === '11') && dateTime.day.value === '31') {
+      alert('Your chosen month doesn\'t have 31 days!')
+      return false;
+    }
+
+    console.log(dateTime.month.value)
+
+    // Check for February 29th
+    if (dateTime.month.value === '02') {
+      var isleap = ( dateTime.year.value % 4 === 0 && (dateTime.year.value % 100 !== 0 || dateTime.year.value % 400 === 0));
+      if (dateTime.day.value > '29' || (dateTime.day.value === '29' && !isleap)) {
+        alert('February ' + dateTime.year.value + ' doesn\'t have ' + dateTime.day.value + ' days!')
+        return false;
+      }
+    }
+
+    alert(constructDate())
+  })
+
+}
+
+(function(){
+  datepicker()
+})();
